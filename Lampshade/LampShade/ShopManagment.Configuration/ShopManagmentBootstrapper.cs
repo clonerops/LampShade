5901ce2d8565c4cd3c6807d2cd5b8a7fb2d58779
masoumisaipa <masoumisaipa@gmail.com>
2023-04-03 11:47:12 +0430

    Configuration ShopManagment

---
+++ Lampshade/LampShade/ShopManagment.Configuration/ShopManagmentBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagment.Application;
+using ShopManagment.Application.Contract.ProductCategory;
+using ShopManagment.Domain.ProductCategoryAgg;
+using ShopManagment.Infrastructure.EFCore;
+using ShopManagment.Infrastructure.EFCore.Repository;
+
+namespace ShopManagment.Configuration
+{
+    public class ShopManagmentBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+            services.AddDbContext<EfContext>(x => x.UseSqlServer(connectionString));
+        }
+    }
+}
