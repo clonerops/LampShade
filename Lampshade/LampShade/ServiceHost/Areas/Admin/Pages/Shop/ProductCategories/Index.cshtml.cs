5604f0f4e45d43c7be8d2d14a58c912653712b46
masoumisaipa <masoumisaipa@gmail.com>
2023-04-16 09:37:29 +0330

    ServiceHost Changes

---
+++ Lampshade/LampShade/ServiceHost/Areas/Admin/Pages/Shop/ProductCategories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Admin.Pages.Shop.ProductCategories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
